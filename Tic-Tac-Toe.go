d599d5af62f7ffdd79cd3f9083fedc23bf653d5f
ali amrol <aliamrol@aut.ac.ir>
2023-07-25 19:19:47 +0330

    debug - range of number - win lose

--- Tic-Tac-Toe.go
+++ Tic-Tac-Toe.go
@@ -6,7 +6,6 @@ import(
 )
 
 func main() {
-
 	fmt.Println("Welcome to Tic-Tac-Toe Game :)")
 	var user1, user2 string
 	fmt.Println("##############################")
@@ -38,29 +37,52 @@ func main() {
 
 
 
-
 	i := 0
 	for {
-		printBoard(board)
-		i++
-		setVal(board, i, player1, player2)
-		status := winLose(board)
-		if status == "X"{
-			fmt.Println("##################################")
-			fmt.Printf("the %s Win the Game :)\n", player1)
+		if i > 8{
+			fmt.Println("HAS NOT WINNER :(")
+			var s int
+			for{
+				fmt.Println("1 - NEW GAME\n2 - EXIT GAME")
+				fmt.Scan(&s)
+				if s == 1 || s == 2{
+					break
+				}else{
+					fmt.Println("PLEASE ENTER THE CORRECT NUMBER")
+				}
+			}
+			if s == 1{
+				i = 0
+				board = [][]string{
+					{" - ", " - ", " - "},
+					{" - ", " - ", " - "},
+					{" - ", " - ", " - "}}
+				tmp := player1
+				player1 = player2
+				player2 = tmp
+			}
+			if s == 2{
+				break
+			}
+		}else {
 			printBoard(board)
-			break
-		}else if status == "O"{
-			fmt.Printf("the %s Win the Game :)\n", player2)
-			printBoard(board)
-			break
+			i++
+			setVal(board, i, player1, player2)
+			status := winLose(board)
+			if status == "X"{
+				fmt.Println("##################################")
+				fmt.Printf("the %s Win the Game :)\n", player1)
+				printBoard(board)
+				break
+			}else if status == "O"{
+				fmt.Printf("the %s Win the Game :)\n", player2)
+				printBoard(board)
+				break
+			}
 		}
-
 	}
-
-
-
 }
+
 var a, b int
 func setVal(board [][]string, i int, pl1 string, pl2 string){
 	if i % 2 == 1{
@@ -68,8 +90,15 @@ func setVal(board [][]string, i int, pl1 string, pl2 string){
 	}else{
 		fmt.Printf("it is your turn %s\n", pl2)
 	}
-	fmt.Println("Please enter the X and Y: ")
-	fmt.Scanf("%d %d", &a, &b)
+	
+	for{
+		fmt.Println("Please enter the X and Y: ")
+		fmt.Scanf("%d %d", &a, &b)
+		if a >= 1 && b >= 1 && a <= 3 && b <= 3 {
+			break
+		}
+		fmt.Println("PLEASE ENTER THE RANGE OF GAME:")
+	}
 	a -= 1
 	b -= 1
 	if i % 2 == 1{
@@ -89,8 +118,6 @@ func printBoard(b [][]string){
 	}
 	fmt.Println("******************************")
 }
-
-
 
 
 func winLose(b [][]string)string{
